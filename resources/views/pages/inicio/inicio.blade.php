7962413800e8b2bb0a4fbe3d2aead2e42d161a46
Aline Neves <pm-est.alineneves>
2025-12-11 09:58:22 -0300

    correção do layout

--- resources/views/pages/inicio/inicio.blade.php
+++ resources/views/pages/inicio/inicio.blade.php
@@ -6,7 +6,7 @@
     <title>Ponto de Registro de Estagiários</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="{{asset('style.css')}}">
 </head>
 <body>
 
@@ -14,7 +14,7 @@
         <div class="card ponto-card">
             
             <div class="text-center pt-4 pb-3">
-                <img src="img/logotipo-dgp.png" alt="Brasão DGP" class="img-fluid brasao-topo">
+                <img src="{{asset('img/logotipo-dgp.png')}}" alt="Brasão DGP" class="img-fluid brasao-topo">
                 <h1 class="ponto-titulo mt-3 mb-4">PONTO DE REGISTRO<br>DE ESTAGIÁRIOS</h1>
             </div>
 
@@ -29,10 +29,10 @@
                 </div>
 
                 <div class="registro-opcoes mb-5">
-                    Primeiro registro do dia: 
-                    <a href="#" id="entradaBtn" class="registro-link entrada-link active">Entrada</a> 
-                    | 
-                    <a href="#" id="saidaBtn" class="registro-link saida-link">Segundo registro. Saída</a>
+                   <p class="text-intro">Primeiro registro do dia: </p> 
+                 <span id="entradaTxt" class="registro-link entrada-link active">Entrada</span> 
+                | 
+                <span id="saidaTxt" class="registro-link saida-link">Segundo registro. Saída</span>
                 </div>
 
                 <div class="d-grid gap-2">
@@ -47,7 +47,7 @@
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="script.js"></script>
+    <script src="{{asset('script.js')}}"></script>
 
 </body>
 </html>
