9abe722b30b4ae524107ceb0f11d8f7efe573e94
est.nicolas <pm-est.nicolaspaiva@pm.pr.gov.br>
2025-12-11 08:12:35 -0300

    criação da pasta pages em resources

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('inicio');
+    return view('welcome');
+});
+Route::get('views/pages', function () {
+    return view('pages.inicio');
 });
