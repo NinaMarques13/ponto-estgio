43711cf6587fe394f7ebcad719163f9b2eb6f713
pm-est.luan <pmpr@DESKTOP-BK93INT.localdomain>
2025-12-11 08:56:16 -0300

    construido tela de login

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('views/pages', function () {
-    return view('pages.inicio');
+Route::get('views/pages/inicio', function () {
+    return view('pages.inicio.inicio');
+});
+
+Route::get('views/adm', function () {
+    return view('adm');
 });
