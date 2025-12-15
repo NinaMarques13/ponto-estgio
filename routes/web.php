6780ae2cfed59b065913553f96246285edb4e002
pm-est.luan <pmpr@DESKTOP-BK93INT.localdomain>
2025-12-15 08:28:02 -0300

    estrutura das pastas

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,8 @@ Route::get('views/pages/inicio', function () {
     return view('pages.inicio.inicio');
 });
 
-Route::get('views/adm', function () {
-    return view('adm');
+Route::get('views/login/adm', function () {
+    return view('pages.login.adm');
 });
 Route::get('/views/pages/inicio', [App\Http\Controllers\EstagiariosController::class, 'index'])->name('inicio.index');
 route::any('/registrar-ponto', [App\Http\Controllers\EstagiariosController::class, 'store'])->name('registrar-ponto');
