a0f24839f1e5aecc021005370b6f32c8a5f183d1
est.nicolas <pm-est.nicolaspaiva@pm.pr.gov.br>
2025-12-15 09:08:52 -0300

    inicio da tela de exportação

---
+++ resources/views/pages/principal/exportacao.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="{{asset('style.css')}}">
+  <title>Planilha de Exportação</title>
+</head>
+
+<body>
+  <header>
+    <div class="img-exp"><img src="{{asset('img/logotipo-dgp.png')}}" alt="logotipo-dgp"></div>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Cadastro de Estagiários</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Planilha de Exportação</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
+  <div>
+
+  </div>
+</body>
+
+</html>
