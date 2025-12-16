b388b92550e3574b369dfd9afba540c2f36b118c
pm-est.luan <pmpr@DESKTOP-BK93INT.localdomain>
2025-12-16 08:55:50 -0300

    Criado tela home relacionado a  ADM

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,9 @@ Route::get('views/pages/inicio', function () {
 Route::get('views/login/adm', function () {
     return view('pages.login.adm');
 });
+
+Route::get('views/principal/dashboard', function () {
+    return view('pages.principal.dashboard');
+});
 Route::get('/views/pages/inicio', [App\Http\Controllers\EstagiariosController::class, 'index'])->name('inicio.index');
 route::any('/registrar-ponto', [App\Http\Controllers\EstagiariosController::class, 'store'])->name('registrar-ponto');
