70f573ee2d8847aaa39f5d9835bd8db4103ffc9d
pm-est.luan <pmpr@DESKTOP-BK93INT.localdomain>
2026-02-05 11:16:57 -0300

    Adicionado modal adicionar estagiario

--- resources/views/pages/principal/cadastro.blade.php
+++ resources/views/pages/principal/cadastro.blade.php
@@ -77,7 +77,48 @@
                         </div>
                     </div>
                 </div>
-            </div> 
+            </div>
+            
+            <div class="modal fade" id="modalAdicionarEstagiario" tabindex="-1" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="modal-content">
+                        <div class="modal-header bg-light">
+                            <h5 class="modal-title text-success">Cadastrar Novo Estagiário</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <form id="formAdicionarEstagiario">
+                            <div class="modal-body">
+                                <div class="row g-3">
+                                    <div class="col-12">
+                                        <label class="form-label fw-bold text-secondary">Nome <span class="text-danger">*</span></label>
+                                        <input type="text" class="form-control" id="nome_cadastro" name="nome" placeholder="Nome completo" required>
+                                    </div>
+                                    <div class="col-12">
+                                        <label class="form-label fw-bold text-secondary">CPF (Matrícula) <span class="text-danger">*</span></label>
+                                        <input type="text" class="form-control" id="cpf_cadastro" name="cpf" placeholder="000.000.000-00" required>
+                                    </div>
+                                    <div class="col-12">
+                                        <label class="form-label fw-bold text-secondary">Setor <span class="text-danger">*</span></label>
+                                        <input type="text" class="form-control" id="setor_cadastro" name="setor" placeholder="Ex: DGP / TI" required>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <label class="form-label fw-bold text-secondary">Telefone</label>
+                                        <input type="tel" class="form-control" id="telefone_cadastro" name="telefone" placeholder="(00) 00000-0000">
+                                    </div>
+                                    <div class="col-md-6">
+                                        <label class="form-label fw-bold text-secondary">Email</label>
+                                        <input type="email" class="form-control" id="email_cadastro" name="email" placeholder="exemplo@pm.pr.gov.br">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="modal-footer bg-light">
+                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                                <button type="submit" class="btn btn-success px-4">Salvar Cadastro</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
 
         <script src="https://cdn.jsdelivr.net/npm/qrcodejs@1.0.0/qrcode.min.js"></script>
     </div>
