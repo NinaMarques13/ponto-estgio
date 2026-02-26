dc21a10cdf513d08b5a27161b3996483e086b94e
est.nicolas <pm-est.nicolaspaiva@pm.pr.gov.br>
2026-02-26 09:32:10 -0300

    migrations atualizadas

---
+++ database/migrations/2025_12_09_121203_create_registro_ponto.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create("registro_ponto", function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('estagiario_id')->constrained('estagiarios')->onDelete('cascade');
+            $table->timestamp('hr_registro');
+            $table->enum('ds_motivo', ['entrada', 'saida', 'folga', 'dispensa', 'recesso', 'falta', 'atestado']);
+            $table->ipAddress('ip_registro');
+            $table->string('ds_observacao', 255)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
