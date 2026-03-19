7f9adba6b99b71402797460e18bf197e4060ff69
dti-est.karen <dti-est.karen@pm.pr.gov.br>
2026-03-19 10:11:42 -0300

    Add do title

--- resources/views/pages/login/adm.blade.php
+++ resources/views/pages/login/adm.blade.php
@@ -1,30 +1,32 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Área Administrativa</title>
-    
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-    
+
     <link rel="stylesheet" href="{{ asset('style.css') }}">
 </head>
 
 <body>
 
     <div class="container-fluid d-flex justify-content-center align-items-center vh-100 p-0 ponto-container">
-        
-        <div class="card p-5 shadow-lg ponto-card" style="max-width: 400px; border-radius: 20px;">
+
+        <div class="card px-4 shadow-lg ponto-card" style="max-width: 550px; border-radius: 20px;">
             <div class="card-body text-center p-0">
-                
-                <div class="logo-placeholder mb-3">
-                    <img src="{{ asset('img/logotipo-dgp.png') }}" alt="Logo DGP" class="img-fluid logo">
+
+                <div class="text-center pt-3 pb-3">
+                    <img src="{{ asset('img/dgp_transparente.png') }}" alt="Brasão DGP" style="width: 40%;"
+                        class="img-fluid brasao-topo" p- title="Brasão da DGP">
                 </div>
-                
-                <h1 class="h5 fw-bold mt-2" style="color: #0E622F;">ÁREA ADMINISTRATIVA</h1>
-                <p class="text-secondary mb-4 small">Sistema de Estagiários</p>
-                
+
+                <h1 class="h5 fw-bold mt-2" style="color: #004A22;" title="Área Administrativa">ÁREA ADMINISTRATIVA</h1>
+                <p class="text-secondary mb-2 small" title="Sistema de Estagiários">Sistema de Estagiários</p>
+
                 <form class="text-start" action="{{ route('admin.login.submit') }}" method="POST">
                     @csrf
 
@@ -38,31 +40,47 @@
                         </div>
                     @endif
                     <div class="mb-3">
-                        <label for="email" class="form-label fw-bold">Usuário</label>
-                        <input type="email" id="usuario" name="email" class="form-control" placeholder="Digite seu e-mail">
-                    </div>
+                        <div class="icon-wrapper">
+                            <svg class="mx-2" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                stroke-linecap="round" stroke-linejoin="round">
+                                <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+                                <circle cx="12" cy="7" r="4"></circle>
+                            </svg>
+                            <label for="email" class="form-label fw-bold" title="Usuário">Usuário</label>
+                            <input type="email" id="usuario" name="email" class="form-control"
+                                placeholder="Digite seu e-mail" title="Digite seu e-mail">
+                        </div>
 
-                    <div class="mb-4">
-                        <label for="senha" class="form-label fw-bold">Senha</label>
-                        <input type="password" id="senha" name="password" class="form-control" placeholder="Digite sua senha">
-                    </div>
+                        <div class="mb-4 mt-4">
+                            <svg class="mx-2" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                stroke-linecap="round" stroke-linejoin="round"
+                                class="lucide lucide-lock-keyhole-icon lucide-lock-keyhole">
+                                <circle cx="12" cy="16" r="1" />
+                                <rect x="3" y="10" width="18" height="12" rx="2" />
+                                <path d="M7 10V7a5 5 0 0 1 10 0v3" />
+                            </svg>
+                            <label for="senha" class="form-label fw-bold" title="Senha">Senha</label>
+                            <input type="password" id="senha" name="password" class="form-control"
+                                placeholder="Digite sua senha" title="Digite sua senha">
+                        </div>
 
-                    <div class="d-grid gap-2 mt-4">
-                        <button type="submit" class="btn btn-lg fw-bold" style="background-color: #0E622F; color: white;">
-                           <i class="bi bi-lock-fill me-2"></i> ENTRAR
-                        </button>
-                    </div>
+                        <div class="d-grid gap-2 mt-4">
+                            <button id="btn_entrar" type="submit" class="btn btn-lg fw-bold" title="Entrar">ENTRAR</button>
+                        </div>
                 </form>
 
-                <p class="text-muted mt-4 small">
+                <p class="text-muted mt-4 mx-2 small">
                     Credenciais padrão: <br>
                     Usuário: **admin@admin.com** | Senha: **admin123**
                 </p>
             </div>
         </div>
-        
+
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
--- resources/views/pages/principal/cadastro.blade.php
+++ resources/views/pages/principal/cadastro.blade.php
@@ -3,9 +3,9 @@
     <div class="tela-cadastro">
         <div class="container my-4">
             <div class="d-flex justify-content-between align-items-center mb-4">
-                <h5 class="text-success fw-normal">Cadastro de Estagiários</h5>
+                <h5 class="text-success fw-normal" title="Cadastro de estagiários">Cadastro de Estagiários</h5>
                 <button class="btn btn-bd-primary px-4 py-2" data-bs-toggle="modal"
-                    data-bs-target="#modalAdicionarEstagiario">Cadastrar estagiário</button>
+                    data-bs-target="#modalAdicionarEstagiario" title="Cadastrar estagiário">Cadastrar estagiário</button>
             </div>
 
             <!-- <div class="row g-3 mb-4">
@@ -23,16 +23,16 @@
                                     </div> -->
 
             <div class="table-responsive shadow-sm">
-                <table id="tabela-estagiarios-cadastrados" class="table table-bordered align-middle mb-4 mt-4 w-100">
+                <table id="tabela-estagiarios-cadastrados" class="table table-bordered align-middle mb-4 mt-4 w-100" title="Tabela de estagiários">
                     <thead class="table-header-custom">
                         <tr>
                             <th class="text-center">id</th>
-                            <th class="text-center">Nome</th>
-                            <th class="text-center">CPF (Matrícula)</th>
-                            <th class="text-center">Setor</th>
-                            <th class="text-center">Telefone</th>
-                            <th class="text-center">Email</th>
-                            <th class="text-center">Ações</th>
+                            <th class="text-center" title="Nome">Nome</th>
+                            <th class="text-center" title="CPF">CPF (Matrícula)</th>
+                            <th class="text-center" title="Setor">Setor</th>
+                            <th class="text-center" title="Telefone">Telefone</th>
+                            <th class="text-center" title="Email">E-mail</th>
+                            <th class="text-center" title="Ações">Ações</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -45,7 +45,7 @@
             <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h5 class="modal-title text-pmp-azul">QR Code do Estagiario</h5>
+                        <h5 class="modal-title text-pmp-azul" title="QR code">QR Code do Estagiario</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body d-flex flex-column align-items-center justify-content-center text-center">
@@ -64,8 +64,8 @@
             <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
                     <div class="modal-header bg-light">
-                        <h5 class="modal-title text-pmp-azul">Cadastrar Novo Estagiário</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        <h5 class="modal-title text-pmp-azul" title="Cadastrar novo estagiário">Cadastrar Novo Estagiário</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" title="Fechar"></button>
                     </div>
                     <form id="formAdicionarEstagiario">
                         @csrf
@@ -74,38 +74,38 @@
                             <div class="row g-3">
                                 <input type="hidden" id="index_edicao" value="">
                                 <div class="col-12">
-                                    <label class="form-label fw-bold text-secondary">Nome <span
+                                    <label class="form-label fw-bold text-secondary" title="Nome">Nome <span
                                             class="text-danger">*</span></label>
                                     <input type="text" class="form-control" id="nome_cadastro" name="nome"
                                         placeholder="Nome completo" required>
                                 </div>
                                 <div class="col-12">
-                                    <label class="form-label fw-bold text-secondary">CPF (Matrícula) <span
+                                    <label class="form-label fw-bold text-secondary" title="CPF">CPF (Matrícula) <span
                                             class="text-danger">*</span></label>
                                     <input type="tel" class="form-control" id="cpf_cadastro" name="cpf"
                                         placeholder="Apenas números" maxlength="11" pattern="\d*" required>
                                 </div>
                                 <div class="col-12">
-                                    <label class="form-label fw-bold text-secondary">Setor <span
+                                    <label class="form-label fw-bold text-secondary" title="Setor">Setor <span
                                             class="text-danger">*</span></label>
                                     <input type="text" class="form-control" id="setor_cadastro" name="setor"
                                         placeholder="Ex: DGP / TI" required>
                                 </div>
                                 <div class="col-md-6">
-                                    <label class="form-label fw-bold text-secondary">Telefone</label>
+                                    <label class="form-label fw-bold text-secondary" title="Telefone">Telefone</label>
                                     <input type="tel" class="form-control" id="telefone_cadastro" name="telefone"
                                         placeholder="(00) 00000-0000" maxlength="11" pattern="\d*">
                                 </div>
                                 <div class="col-md-6">
-                                    <label class="form-label fw-bold text-secondary">Email</label>
+                                    <label class="form-label fw-bold text-secondary" title="Email">E-mail</label>
                                     <input type="email" class="form-control" id="email_cadastro" name="email"
                                         placeholder="exemplo@pm.pr.gov.br">
                                 </div>
                             </div>
                         </div>
                         <div class="modal-footer bg-light">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-                            <button type="submit" class="btn btn-bd-primary px-4">Salvar Cadastro</button>
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" title="Cancelar">Cancelar</button>
+                            <button type="submit" class="btn btn-bd-primary px-4" title="Salvar">Salvar Cadastro</button>
                         </div>
                     </form>
                 </div>
@@ -116,7 +116,7 @@
             <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
                     <div class="modal-header bg-light">
-                        <h5 class="modal-title text-pmp-azul">Editar Informações do Estagiário</h5>
+                        <h5 class="modal-title text-pmp-azul" title="Editar Estagiario">Editar Informações do Estagiário</h5>
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <form id="formEditarEstagiario">
@@ -125,35 +125,35 @@
                             <div class="row g-3">
                                 <input type="hidden" id="id_estagiario_editar" name="id_estagiario_editar">
                                 <div class="col-12">
-                                    <label class="form-label fw-bold text-secondary">Nome <span
+                                    <label class="form-label fw-bold text-secondary" title="Nome">Nome <span
                                             class="text-danger">*</span></label>
                                     <input type="text" class="form-control" id="nome_editar" required>
                                 </div>
                                 <div class="col-12">
-                                    <label class="form-label fw-bold text-secondary">CPF (Matrícula) <span
+                                    <label class="form-label fw-bold text-secondary" title="CPF">CPF (Matrícula) <span
                                             class="text-danger">*</span></label>
                                     <input type="tel" class="form-control" id="cpf_editar" maxlength="11" pattern="\d*"
                                         required>
                                 </div>
                                 <div class="col-12">
-                                    <label class="form-label fw-bold text-secondary">Setor <span
+                                    <label class="form-label fw-bold text-secondary" title="Setor">Setor <span
                                             class="text-danger">*</span></label>
                                     <input type="text" class="form-control" id="setor_editar" required>
                                 </div>
                                 <div class="col-md-6">
-                                    <label class="form-label fw-bold text-secondary">Telefone</label>
+                                    <label class="form-label fw-bold text-secondary" title="Telefone">Telefone</label>
                                     <input type="tel" class="form-control" id="telefone_editar" maxlength="11"
                                         pattern="\d*">
                                 </div>
                                 <div class="col-md-6">
-                                    <label class="form-label fw-bold text-secondary">Email</label>
+                                    <label class="form-label fw-bold text-secondary" title="Email">E-mail</label>
                                     <input type="email" class="form-control" id="email_editar">
                                 </div>
                             </div>
                         </div>
                         <div class="modal-footer bg-light">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-                            <button type="submit" class="btn btn-bd-primary px-4">Atualizar Cadastro</button>
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" title="Cancelar">Cancelar</button>
+                            <button type="submit" class="btn btn-bd-primary px-4" title="Atualizar cadastro">Atualizar Cadastro</button>
                         </div>
                     </form>
                 </div>
