19405c813162f354e22e5e7db1671b8d30bd0b11
est.nicolas <pm-est.nicolaspaiva@pm.pr.gov.br>
2026-03-20 09:14:19 -0300

    20/03

--- database/migrations/2025_12_01_000000_create_estagiarios_table.php
+++ database/migrations/2025_12_01_000000_create_estagiarios_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -28,5 +29,5 @@ return new class extends Migration {
     public function down(): void
     {
         Schema::dropIfExists('estagiarios');
-    }
+    } 
 };
