cf222113c16c41ac643c9a8567a4f2af8abe2709
KAREN <pm-est.bisson@pm.pr.gov.br>
2026-04-15 09:44:44 -0300

    Jquery local

--- resources/views/pages/templates/layout.blade.php
+++ resources/views/pages/templates/layout.blade.php
@@ -68,7 +68,7 @@
           </div>
         </div>
 
-  <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+  <script src="{{ asset('js/vendor/jquery-4.0.0.min.js') }}"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
   <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
   <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
