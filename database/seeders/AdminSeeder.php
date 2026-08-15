fa4fc61c8877b3819d4f789f43cbc38899ddb1cf
Ninamarques13 <nicolaspaiva2001@gmail.com>
2026-08-14 22:41:36 -0300

    Usa updateOrCreate no AdminSeeder para evitar duplicados

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -12,28 +12,34 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-        Admin::create([
-            'cpf' => '12345678904',
-            'name' => 'Super Admin',
-            'email' => 'admin@admin.com',
-            'password' => Hash::make('adm123'),
-            'level' => 1,
-        ]);
+        Admin::updateOrCreate(
+            ['cpf' => '12345678904'],
+            [
+                'name' => 'Super Admin',
+                'email' => 'admin@admin.com',
+                'password' => Hash::make('adm123'),
+                'level' => 1,
+            ]
+        );
 
-        Admin::create([
-            'cpf' => '12345678901',
-            'name' => 'Admin Comum 1',
-            'email' => 'admin1@admin.com',
-            'password' => Hash::make('adm123'),
-            'level' => 2,
-        ]);
+        Admin::updateOrCreate(
+            ['cpf' => '12345678901'],
+            [
+                'name' => 'Admin Comum 1',
+                'email' => 'admin1@admin.com',
+                'password' => Hash::make('adm123'),
+                'level' => 2,
+            ]
+        );
 
-        Admin::create([
-            'cpf' => '12345678902',
-            'name' => 'Admin Comum 2',
-            'email' => 'admin2@admin.com',
-            'password' => Hash::make('adm123'),
-            'level' => 2,
-        ]);
+        Admin::updateOrCreate(
+            ['cpf' => '12345678902'],
+            [
+                'name' => 'Admin Comum 2',
+                'email' => 'admin2@admin.com',
+                'password' => Hash::make('adm123'),
+                'level' => 2,
+            ]
+        );
     }
 }
